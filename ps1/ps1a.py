a35a17e5a6f0f8550bfc7deb3bdb8c801c678f1f
Ishan Mukherjee <ishanjmukherjee@gmail.com>
2022-04-25 00:31:56 +0530

    Update ps1a.py

--- ps1/ps1a.py
+++ ps1/ps1a.py
@@ -1,6 +1,6 @@
-total_cost = float(input("Total cost: "))
-portion_saved = float(input("Portion saved: "))
-annual_salary = float(input("Annual salary: "))
+annual_salary = float(input("Enter your annual salary: "))
+portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
+total_cost = float(input("Enter the cost of your dream home: "))
 monthly_salary = annual_salary / 12
 
 r = 0.04
@@ -9,8 +9,8 @@ current_savings = 0
 
 months = 0
 
-while current_savings < total_cost * portion_down_payment:
+while current_savings < portion_down_payment:
     current_savings += monthly_salary * portion_saved + current_savings * r / 12
     months += 1
     
-print(months, "months required")
+print("Number of months:", months)
